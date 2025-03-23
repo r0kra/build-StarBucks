4795fabe76753d0368c9a4b1d3a2d9d124767837
Roman Kravchuk <roman.kravchuk5k@gmail.com>
2025-03-23 17:17:12 +0200

    Delete assets directory

--- assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('#btn-burger');
-const mobileContainer = document.querySelector('#mobile-container');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  mobileContainer.classList.toggle('show');
-});
